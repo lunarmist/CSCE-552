835a7b4c83b66ab1d9f818f92bdb18bb52534ecd
Abdullah Bueno <abueno@email.sc.edu>
2023-12-01 13:37:31 -0800

    Gameover & timer
    Added game over screen and timer to game

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime <= 0)
+        {
+            remainingTime = 0;
+            gameOver();
+        }
+
+        if (remainingTime < 10)
+        {
+            timerText.color = Color.red;
+        }
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        
+    }
+
+    void gameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -14,4 +14,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void retry()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
